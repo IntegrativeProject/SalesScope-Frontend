40790872b433569565d44dc8e56b5b3608df1ec5
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-26 09:43:06 -0500

    navbar crated

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,16 @@
-import Card from '@/components/ui/Card'
-import React from 'react'
+import Card from "@/components/ui/Card";
+import Navbar from "@/components/ui/Navbar";
+import React from "react";
 
 export default function page() {
   return (
-    <div className='flex justify-center items-center mt-50 bg-[#F5F6FA]'>
-      <Card/>
+    <div>
+      
+      
+
+      <div className="flex justify-center items-center mt-50 bg-[#F5F6FA]">
+        <Card />
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='bg-white h-screen w-60'>Sidebar</div>
+  )
+}
