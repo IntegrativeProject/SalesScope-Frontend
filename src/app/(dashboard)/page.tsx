9f0a445cd368a87020d48853af35b25069e259c4
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-26 09:15:44 -0500

    card component created

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import Card from '@/components/ui/Card'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex justify-center items-center mt-50 bg-[#F5F6FA]'>
+      <Card/>
+    </div>
+  )
+}
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className='bg-white w-64 h-40 rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)]  ' >
+        <h1 className='text-gray-500 p-4 '>Total Order</h1>
+        <h1 className='font-bold ml-4 text-3xl'>10293</h1>
+        <h3 className=' text-gray-500 p-4 mt-3 ml-6' >1.3% Up from past week</h3>
+    </div>
+  )
+}
