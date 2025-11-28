946bb51ff35f975744ec85b6455fbea82a66ae98
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-28 09:18:45 -0500

    feature(Asidebar)routes added to the elements

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,11 +1,58 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Sidebar({ className }: { className?: string }) {
+  const pathname = usePathname();
   return (
-    <aside className={`h-screen p-4 ${className}`}>
-      <ul className="menu">
-        <li><a>Dashboard</a></li>
-        <li><a>Users</a></li>
-        <li><a>Settings</a></li>
-      </ul>
-    </aside>
+    <aside className="w-64 bg-white border-r border-gray-200 flex flex-col">
+        <div className="h-20 flex items-center justify-center border-b border-gray-200">
+          <h2 className="text-xl font-semibold">Logo</h2>
+        </div>
+
+        <nav className="flex-1 p-6 space-y-2">
+          <Link
+            href="/"
+            className={`block px-4 py-2 rounded-lg text-sm font-medium ${
+              pathname === "/"
+                ? "bg-[#4880FF] text-white"
+                : "hover:bg-[#4880FF]"
+            }`}
+          >
+            Dashboard
+          </Link>
+
+          <Link
+            href="/registersale"
+            className={`block px-4 py-2 rounded-lg text-sm font-medium ${
+              pathname === "/registersale"
+                ? "bg-[#4880FF] text-white"
+                : "hover:bg-gray-100"
+            }`}
+          >
+            Register sale
+          </Link>
+          <Link
+            href="/products"
+            className={`block px-4 py-2 rounded-lg text-sm font-medium ${
+              pathname === "/products"
+                ? "bg-[#4880FF] text-gray-900"
+                : "hover:bg-gray-100"
+            }`}
+          >
+            Products
+          </Link>
+          <Link
+            href="/analysisandpredictions"
+            className={`block px-4 py-2 rounded-lg text-sm font-medium ${
+              pathname === "/analysisandpredictions"
+                ? "bg-[#4880FF] text-gray-900"
+                : "hover:bg-gray-100"
+            }`}
+          >
+            Analysis and Predictions
+          </Link>
+        </nav>
+      </aside>
   );
 }
