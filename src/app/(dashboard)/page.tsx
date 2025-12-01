e71d6d5b811e6c4756198d855afebfef52bcc261
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-01 07:59:54 -0500

    structure from cards added in card props

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -6,10 +6,10 @@ export default function page() {
     <div>
       <h1 className="p-1 ml-9 font-bold  text-4xl">Dashboard</h1>
       <div className=" ml-11 grid grid-cols-4 gap-x-3 bg-[#F5F6FA] mt-5 ">
-          <Card w={78} h={50} mainText="Total Sales" worth={36.200} percentage="12.5%" endText=" vs previous week"/>
-          <Card w={78} h={50} mainText="Daily Avarage" worth={5.171} percentage="18.3%" endText="vs previous week"/>
+          <Card w={78} h={50} mainText="Total Sales" worth={36.200} percentage="12.5%" endText=" vs previous week" iconRoute="/icons/ventas.png" simbol={true}/>
+          <Card w={78} h={50} mainText="Daily Avarage" worth={5.171} percentage="18.3%" endText="vs previous week" iconRoute="/icons/promedio.png" simbol={true}/>
           <Card w={78} h={50} mainText="Products Sold" worth={362} percentage="15.2%" endText="vs previous week" iconRoute="/icons/agregar-producto.png"/>
-          <Card w={78} h={50} mainText="Transaction" worth={89} percentage="9.8%" endText="vs previous week" />
+          <Card w={78} h={50} mainText="Transaction" worth={89} percentage="9.8%" endText="vs previous week" iconRoute="/icons/transaccion2.png"/>
       </div>
     </div>
   );
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -8,19 +8,20 @@ type CardProps = {
   mainText?:string
   endText?:string
   iconRoute?:string
+  simbol?:boolean
 }
 
 
-export default function Card({w,h,worth,percentage,mainText,endText,iconRoute}:CardProps) {
+export default function Card({w,h,worth,percentage,mainText,endText,iconRoute,simbol}:CardProps) {
 
   return (
     <div className={`relative bg-white w-${w} h-${h} rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 `}  >
         <h1 className='text-gray-500 p-4 '>{mainText}</h1>
         <div className='flex ' >
-        <h1 className='font-bold  text-4xl ml-10'>{worth}</h1>
-        <img src={`${iconRoute}`} alt="icon" className='w-15 h-15 absolute top-4 right-4 '/>
+        <h1 className='font-bold  text-4xl ml-15 mt-4 '>{simbol? "$":""}{worth}</h1>
+        <img src={`${iconRoute}`} alt="icon" className='w-17 h-17 absolute top-4 right-4 mr-4 mt-2'/>
         </div>
-        <h3 className=' text-gray-500 p-4 mt-3 ml-6' >{percentage}{endText}</h3>
+        <h3 className=' text-gray-500 p-4 mt- ml-8' >{percentage}{endText}</h3>
     </div>
   )
 }
