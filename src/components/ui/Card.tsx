a4d8808e6ad3939cd0832a9c4694bd767074cf2c
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-01 07:34:50 -0500

    icons added

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -7,15 +7,19 @@ type CardProps = {
   percentage?:string
   mainText?:string
   endText?:string
+  iconRoute?:string
 }
 
 
-export default function Card({w,h,worth,percentage,mainText,endText}:CardProps) {
+export default function Card({w,h,worth,percentage,mainText,endText,iconRoute}:CardProps) {
 
   return (
-    <div className={`bg-white w-${w} h-${h} rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 `}  >
+    <div className={`relative bg-white w-${w} h-${h} rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 `}  >
         <h1 className='text-gray-500 p-4 '>{mainText}</h1>
-        <h1 className='font-bold ml-4 text-3xl'>{worth}</h1>
+        <div className='flex ' >
+        <h1 className='font-bold  text-4xl ml-10'>{worth}</h1>
+        <img src={`${iconRoute}`} alt="icon" className='w-15 h-15 absolute top-4 right-4 '/>
+        </div>
         <h3 className=' text-gray-500 p-4 mt-3 ml-6' >{percentage}{endText}</h3>
     </div>
   )
