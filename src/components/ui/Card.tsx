88134815a3c0455fd775da13e62677190b2641f9
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-02 06:41:55 -0500

    feat(listContainer) list with sections to products created

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,27 +1,46 @@
-import React from 'react'
+import React from "react";
 
 type CardProps = {
-  w?:number,
-  h?:number
-  worth?:number
-  percentage?:string
-  mainText?:string
-  endText?:string
-  iconRoute?:string
-  simbol?:boolean
-}
+  w?: number;
+  h?: number;
+  worth?: number;
+  percentage?: string;
+  mainText?: string;
+  endText?: string;
+  iconRoute?: string;
+  simbol?: boolean;
+};
 
-
-export default function Card({w,h,worth,percentage,mainText,endText,iconRoute,simbol}:CardProps) {
-
+export default function Card({
+  w,
+  h,
+  worth,
+  percentage,
+  mainText,
+  endText,
+  iconRoute,
+  simbol,
+}: CardProps) {
   return (
-    <div className={`relative bg-white w-${w} h-${h} rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 `}  >
-        <h1 className='text-gray-500 p-4 '>{mainText}</h1>
-        <div className='flex ' >
-        <h1 className='font-bold  text-4xl ml-15 mt-4 '>{simbol? "$":""}{worth}</h1>
-        <img src={`${iconRoute}`} alt="icon" className='w-17 h-17 absolute top-4 right-4 mr-4 mt-2'/>
-        </div>
-        <h3 className=' text-gray-500 p-4 mt- ml-8' >{percentage}{endText}</h3>
+    <div
+      className={`relative bg-white w-${w} h-${h} rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 `}
+    >
+      <h1 className="text-gray-500 p-4 ">{mainText}</h1>
+      <div className="flex ">
+        <h1 className="font-bold  text-4xl ml-15 mt-4 ">
+          {simbol ? "$" : ""}
+          {worth}
+        </h1>
+        <img
+          src={`${iconRoute}`}
+          alt="icon"
+          className="w-17 h-17 absolute top-4 right-4 mr-4 mt-2"
+        />
+      </div>
+      <h3 className=" text-gray-500 p-4 mt-8 ml-8">
+        {percentage}
+        {endText}
+      </h3>
     </div>
-  )
+  );
 }
--- src/components/ui/ListContainer.tsx
+++ src/components/ui/ListContainer.tsx
@@ -7,7 +7,7 @@ type ListProps = {
 
 export default function ListContainer({ mainTitle, price }: ListProps) {
   return (
-    <section className="bg-white w-140 h-150 rounded-xl p-4 shadow">
+    <section className="bg-white w-140 h-150 rounded-xl p-4 shadow space-y-5 ">
       <h1 className="font-bold text-lg mb-4">Latest Sales</h1>
 
       <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
@@ -19,8 +19,78 @@ export default function ListContainer({ mainTitle, price }: ListProps) {
             <p className="">24 Nov 2025</p>
           </div>
 
-          <div className="flex items-center  ">
+          <div className="flex items-center ml-28  ">
             <p className="font-bold text-lg">$8481</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
+        <h2 className="font-bold text-base ">Product A</h2>
+
+        <div className="flex space-x-40">
+          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
+            <p className="">Amount: 3</p>
+            <p className="">24 Nov 2025</p>
+          </div>
+
+          <div className="flex items-center  ">
+            <p className="font-bold text-lg ml-28">$8481</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
+        <h2 className="font-bold text-base ">Product A</h2>
+
+        <div className="flex space-x-40">
+          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
+            <p className="">Amount: 3</p>
+            <p className="">24 Nov 2025</p>
+          </div>
+
+          <div className="flex items-center  ">
+            <p className="font-bold text-lg ml-28">$8481</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
+        <h2 className="font-bold text-base ">Product A</h2>
+
+        <div className="flex space-x-40">
+          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
+            <p className="">Amount: 3</p>
+            <p className="">24 Nov 2025</p>
+          </div>
+
+          <div className="flex items-center  ">
+            <p className="font-bold text-lg ml-28">$8481</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
+        <h2 className="font-bold text-base ">Product A</h2>
+
+        <div className="flex space-x-40">
+          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
+            <p className="">Amount: 3</p>
+            <p className="">24 Nov 2025</p>
+          </div>
+
+          <div className="flex items-center  ">
+            <p className="font-bold text-lg ml-28">$8481</p>
+          </div>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
+        <h2 className="font-bold text-base ">Product A</h2>
+
+        <div className="flex space-x-40">
+          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
+            <p className="">Amount: 3</p>
+            <p className="">24 Nov 2025</p>
+          </div>
+
+          <div className="flex items-center  ">
+            <p className="font-bold text-lg ml-28">$8481</p>
           </div>
         </div>
       </article>
