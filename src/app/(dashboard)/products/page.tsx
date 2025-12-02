65298a1025c13b68d4f630bbc7e5922a7da7b287
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-02 07:02:26 -0500

    feat(productsPage) cards desing added

--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import Card from "@/components/ui/Card";
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <div className="flex justify-center space-x-30 ">
+        <Card w={80} h={50} mainText="Total Products" iconRoute="/icons/agregar-producto.png"/>
+        <Card w={80} h={50} mainText="Total Inventory Value" iconRoute="/icons/inventario.png"/>
+        <Card w={80} h={50} mainText="Low Stock" iconRoute="/icons/nohaystock.png"/>
+      </div>
+    </div>
+  );
 }
