b63e40f5efe8f65e1f6f07e510596ca0d8576e39
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-03 06:50:56 -0500

    feat(analysisCards):props and border color added

--- src/app/(dashboard)/analysisandpredictions/page.tsx
+++ src/app/(dashboard)/analysisandpredictions/page.tsx
@@ -5,14 +5,20 @@ export default function page() {
   return (
     <div className="flex space-x-20 justify-center  ">
       <AnalysisCards
+        footerText="Prediction based on the last 3 weeks"
+        price={30.067}
         mainText="Moving Average (3 weeks)"
         borderColor="border-[#4880FF]"
       />
       <AnalysisCards
+        footerText="Prediction based on the last 3 weeks"
+        price={28.400}
         mainText="Moving Average (5 weeks)"
         borderColor="border-[#00D824]"
       />
       <AnalysisCards
+        footerText="Prediction based on the last 3 weeks"
+        price={33.436}
         mainText="Linear Regression"
         borderColor="border-[#CD07E3]"
       />
--- src/components/ui/AnalysisCards.tsx
+++ src/components/ui/AnalysisCards.tsx
@@ -4,26 +4,26 @@ import React from "react";
 type CardProps ={
     borderColor:string
     mainText:string
+    price:number
+    footerText:string
 
 }
 
 
-export default function AnalysisCards({borderColor,mainText}:CardProps) {
+export default function AnalysisCards({borderColor,mainText,price,footerText}:CardProps) {
   return (
     <div
-      className={`relative bg-white w-95 h-55 rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 border-l-7 ${borderColor} `}
+      className={`relative bg-white w-95 h-50 rounded-2xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] hover:scale-101 border-l-7 ${borderColor} `}
     >
       <h1 className="text-gray-500 p-4 ">{mainText}</h1>
       <div className="flex ">
-        <h1 className="font-bold  text-4xl ml-15 mt-4 ">
-          
-        </h1>
+      
         
       </div>
-      <h3 className=" text-gray-500 p-4 mt-8 ml-8">
-        <h1>dsa</h1>
-        <h1>fsf</h1>
-      </h3>
+      <div className="  p-4 mt-10 ml-1">
+        <h1 className="font-bold text-2xl mb-5 ">{`$${price}`}</h1>
+        <h1 className="text-sm text-gray-500" >{footerText}</h1>
+      </div>
     </div>
   );
 }
