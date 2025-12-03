187aa40d2d032a8cf8569c015f9ad6e57c3b8f2e
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-03 07:09:46 -0500

    feat(recommendationsCard): tittle and subtittle added

--- src/components/ui/RecommendationsCard.tsx
+++ src/components/ui/RecommendationsCard.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function RecommendationsCard() {
   return (
     <div className='bg-white w-300 h-80 rounded-2xl border-l-7 border-[#ff6f00b0]' >
-
+      <h1 className='mt-4 ml-4 text-2xl font-bold ' >Recommendations</h1>
+      <p className='text-[#202224ce] ml-4 font-semibold ' >Suggested actions based on the analysis</p>
 
     </div>
   )
