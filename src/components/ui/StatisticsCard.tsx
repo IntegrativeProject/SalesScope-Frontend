767adb5bfc3be5dd146f025843c9313f545a1c06
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-03 09:07:13 -0500

    feat(statisticsCard): container and elements whit icons and texts was created

--- src/components/ui/StatisticsCard.tsx
+++ src/components/ui/StatisticsCard.tsx
@@ -1,96 +1,88 @@
-import React from 'react'
+import React from "react";
 
 export default function StatisticsCard() {
   return (
     <section className="bg-white w-140 mt-5 h-auto rounded-xl p-4 space-y-5 shadow-xl border border-gray-200 mb-5">
       <h1 className="text-center text-xl font-bold text-black">Latest Sales</h1>
 
-      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
-        <h2 className="font-bold text-base ">Product A</h2>
-
-        <div className="flex space-x-50">
-          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
-            <img src="/icons/arriba.png" alt="" className='w-6' />
-            <p className="">145 units this week</p>
-          </div>
-
-          <div className="flex items-center  ">
-            <p className="font-bold text-lg">$8481</p>
+      <article className="bg-[#FBFCFF] flex  h-auto border border-[#D5D5D5] rounded-xl p-2">
+        <div className="grid mt-auto gap-3 text-sm font-medium  ">
+          <h2 className="font-bold mb-1 text-xl ml-1">Product A</h2>
+          <div className="flex space-x-1">
+            <img src="/icons/arriba.png" alt="" className="w-6" />
+            <p className="text-[#ADADAD] ">145 units this week</p>
           </div>
         </div>
-      </article>
-      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
-        <h2 className="font-bold text-base ">Product A</h2>
 
-        <div className="flex space-x-50">
-          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
-            <img src="/icons/arriba.png" alt="" className='w-6' />
-            <p className="">145 units this week</p>
-          </div>
-
-          <div className="flex items-center  ">
-            <p className="font-bold text-lg">$8481</p>
-          </div>
+        <div className="w-15  ml-auto  flex items-center">
+          <p className=" bg-[#3ae98063] text-green-600 rounded-xl p-1">$8481</p>
         </div>
       </article>
-      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
-        <h2 className="font-bold text-base ">Product A</h2>
-
-        <div className="flex space-x-50">
-          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
-            <img src="/icons/decrecer.png" alt="" className='w-6' />
-            <p className="">145 units this week</p>
-          </div>
-
-          <div className="flex items-center  ">
-            <p className="font-bold text-lg">$8481</p>
+      <article className="bg-[#FBFCFF] flex  h-auto border border-[#D5D5D5] rounded-xl p-2">
+        <div className="grid mt-auto gap-3 text-sm font-medium  ">
+          <h2 className="font-bold mb-1 text-xl ml-1">Product A</h2>
+          <div className="flex space-x-1">
+            <img src="/icons/arriba.png" alt="" className="w-6" />
+            <p className="text-[#ADADAD] ">145 units this week</p>
           </div>
         </div>
-      </article>
-      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
-        <h2 className="font-bold text-base ">Product A</h2>
 
-        <div className="flex space-x-50">
-          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
-            <img src="/icons/arriba.png" alt="" className='w-6' />
-            <p className="">145 units this week</p>
-          </div>
-
-          <div className="flex items-center  ">
-            <p className="font-bold text-lg">$8481</p>
-          </div>
+        <div className="w-15  ml-auto  flex items-center">
+          <p className=" bg-[#3ae98063] text-green-600 rounded-xl p-1">$8481</p>
         </div>
       </article>
-      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
-        <h2 className="font-bold text-base ">Product A</h2>
-
-        <div className="flex space-x-50">
-          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
-            <img src="/icons/decrecer.png" alt="" className='w-6' />
-            <p className="">145 units this week</p>
-          </div>
-
-          <div className="flex items-center  ">
-            <p className="font-bold text-lg">$8481</p>
+      <article className="bg-[#FBFCFF] flex  h-auto border border-[#D5D5D5] rounded-xl p-2">
+        <div className="grid mt-auto gap-3 text-sm font-medium  ">
+          <h2 className="font-bold mb-1 text-xl ml-1">Product A</h2>
+          <div className="flex space-x-1">
+            <img src="/icons/decrecer.png" alt="" className="w-6" />
+            <p className="text-[#ADADAD] ">145 units this week</p>
           </div>
         </div>
-      </article>
-      <article className="bg-[#FBFCFF] border border-[#D5D5D5] rounded-xl p-2">
-        <h2 className="font-bold text-base ">Product A</h2>
 
-        <div className="flex space-x-50">
-          <div className="flex items-center mt-auto flex-col-2 gap-3 text-sm text-[#ADADAD] font-medium  ">
-            <img src="/icons/decrecer.png" alt="" className='w-6' />
-            <p className="">145 units this week</p>
-          </div>
-
-          <div className="flex items-center  ">
-            <p className="font-bold text-lg">$8481</p>
-          </div>
+        <div className="w-15  ml-auto  flex items-center">
+          <p className=" bg-[#e93a3a5b] text-red-600 rounded-xl p-1">$8481</p>
         </div>
       </article>
-     
-      
+      <article className="bg-[#FBFCFF] flex  h-auto border border-[#D5D5D5] rounded-xl p-2">
+        <div className="grid mt-auto gap-3 text-sm font-medium  ">
+          <h2 className="font-bold mb-1 text-xl ml-1">Product A</h2>
+          <div className="flex space-x-1">
+            <img src="/icons/arriba.png" alt="" className="w-6" />
+            <p className="text-[#ADADAD] ">145 units this week</p>
+          </div>
+        </div>
+
+        <div className="w-15  ml-auto  flex items-center">
+          <p className=" bg-[#3ae98063] text-green-600 rounded-xl p-1">$8481</p>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] flex  h-auto border border-[#D5D5D5] rounded-xl p-2">
+        <div className="grid mt-auto gap-3 text-sm font-medium  ">
+          <h2 className="font-bold mb-1 text-xl ml-1">Product A</h2>
+          <div className="flex space-x-1">
+            <img src="/icons/decrecer.png" alt="" className="w-6" />
+            <p className="text-[#ADADAD] ">145 units this week</p>
+          </div>
+        </div>
+
+        <div className="w-15  ml-auto  flex items-center">
+          <p className=" bg-[#e93a3a5b] text-red-600  rounded-xl p-1">$8481</p>
+        </div>
+      </article>
+      <article className="bg-[#FBFCFF] flex  h-auto border border-[#D5D5D5] rounded-xl p-2">
+        <div className="grid mt-auto gap-3 text-sm font-medium  ">
+          <h2 className="font-bold mb-1 text-xl ml-1">Product A</h2>
+          <div className="flex space-x-1">
+            <img src="/icons/arriba.png" alt="" className="w-6" />
+            <p className="text-[#ADADAD] ">145 units this week</p>
+          </div>
+        </div>
+
+        <div className="w-15  ml-auto  flex items-center">
+          <p className=" bg-[#3ae98063] text-green-600 rounded-xl p-1">$8481</p>
+        </div>
+      </article>
     </section>
-  )
+  );
 }
