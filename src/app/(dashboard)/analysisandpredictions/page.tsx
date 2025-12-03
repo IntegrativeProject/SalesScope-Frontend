91ed8c7b894eaf9e6566a48fec37ca6b5121ff35
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-03 07:02:19 -0500

    feat(recommendationsCard): sixe fixed and border color added

--- src/app/(dashboard)/analysisandpredictions/page.tsx
+++ src/app/(dashboard)/analysisandpredictions/page.tsx
@@ -1,27 +1,35 @@
 import AnalysisCards from "@/components/ui/AnalysisCards";
+import RecommendationsCard from "@/components/ui/RecommendationsCard";
 import React from "react";
 
 export default function page() {
   return (
-    <div className="flex space-x-20 justify-center  ">
-      <AnalysisCards
-        footerText="Prediction based on the last 3 weeks"
-        price={30.067}
-        mainText="Moving Average (3 weeks)"
-        borderColor="border-[#4880FF]"
-      />
-      <AnalysisCards
-        footerText="Prediction based on the last 3 weeks"
-        price={28.400}
-        mainText="Moving Average (5 weeks)"
-        borderColor="border-[#00D824]"
-      />
-      <AnalysisCards
-        footerText="Prediction based on the last 3 weeks"
-        price={33.436}
-        mainText="Linear Regression"
-        borderColor="border-[#CD07E3]"
-      />
+    <div>
+      <div className="flex space-x-20 justify-center  ">
+        <AnalysisCards
+          footerText="Prediction based on the last 3 weeks"
+          price={30.067}
+          mainText="Moving Average (3 weeks)"
+          borderColor="border-[#4880FF]"
+        />
+        <AnalysisCards
+          footerText="Prediction based on the last 3 weeks"
+          price={28.4}
+          mainText="Moving Average (5 weeks)"
+          borderColor="border-[#00D824]"
+        />
+        <AnalysisCards
+          footerText="Prediction based on the last 3 weeks"
+          price={33.436}
+          mainText="Linear Regression"
+          borderColor="border-[#CD07E3]"
+        />
+      </div>
+
+      <div className="flex justify-center">
+
+      <RecommendationsCard/>
+      </div>
     </div>
   );
 }
