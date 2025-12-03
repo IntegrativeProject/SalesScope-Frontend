7a077add9843584fae1c2df6e0ba69470eff9fb0
Nayib0 <nayibobeso@gmail.com>
2025-12-03 16:14:07 -0500

    feat(BestDayCard):  card layout was created

--- src/components/ui/BestDayCard.tsx
+++ src/components/ui/BestDayCard.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
 export default function BestDayCard() {
   return (
-    <div className="card">
-      
+    <div className="bg-white w-260 h-auto rounded-xl p-4 space-y-5 shadow-xl border-gray-200">
+      <article className="p-5 bg-white rounded-xl shadow w-[300px]">
+        <h2 className="text-lg font-semibold">
+          Best Day: <strong className="text-green-600">Sat</strong>
+        </h2>
+        <p className="text-sm text-gray-700">Average sales of $7,200</p>
+      </article>
     </div>
-  )
+  );
 }
-
-
