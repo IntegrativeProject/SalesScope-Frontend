911b4d73d0bc17c1b176515ba997bf1bc267c8fb
nayib0 <nayibobeso@gmail.com>
2025-12-04 10:22:32 -0500

    feat(PredictionCard):Prediction card created and restructuring of the main page

---
+++ src/components/ui/PredictionCard.tsx
@@ -0,0 +1,14 @@
+
+  export default function PredictionCard() {
+    return (
+      <section className="bg-white mt-5 mb-5 w-270 h-120 rounded-xl p-6 shadow-xl border border-gray-200 flex flex-col justify-end ">
+        <article className="p-3 bg-blue-200 rounded-xl shadow border border-blue-300">
+          <h2 className="text-lg font-semibold">
+            Prediction for the coming weeks:
+          </h2>
+          <p className="text-sm text-gray-600 font-semibold">The last few week show a growth trend of 90%. It is recommended to increase stock levels of popular products</p>
+        </article>
+      </section>
+    );
+  }
+  
