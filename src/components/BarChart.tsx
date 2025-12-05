9a61abadbf452114f41d530f12a5fe26608123ea
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-05 07:57:55 -0500

    chart librery added

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+
+export default function BarChart() {
+  const data = {
+    labels: ["Monday", "Tuesday", "Wednesday" , "Thursday" ,"Friday","Saturday","Sunday"],
+    datasets: [
+      {
+        label: "Ventas",
+        data: [150, 230, 180,200,190,120,185],
+        backgroundColor: "#4880FF",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { position: "top" as const },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+}
