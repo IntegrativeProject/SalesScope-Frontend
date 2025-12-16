8b4f0ce1e065608063c328ceb5264a6689743a11
cristianpy09 <cristiancelismoreno@gmail.com>
2025-12-15 23:02:50 -0500

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,6 @@ export async function middleware(req: NextRequest) {
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  // Rutas protegidas
   if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
